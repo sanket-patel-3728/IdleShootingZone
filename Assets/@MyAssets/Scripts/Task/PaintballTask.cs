b7bd9009eb600f854ab1547642b02b032579144b
Umang213 <umangpatel0321@gmail.com>
2023-03-15 18:43:07 +0530

    15/3/23

--- Assets/@MyAssets/Scripts/Task/PaintballTask.cs
+++ Assets/@MyAssets/Scripts/Task/PaintballTask.cs
@@ -7,6 +7,7 @@ public class PaintballTask : TaskController
     public bool isBarrel;
     public override void StartTask()
     {
+        storedCustomer.transform.position = taskPoint.position;
         if (isBarrel)
         {
             storedCustomer.SetAnimation("BarrelIdle", true);
@@ -15,5 +16,48 @@ public class PaintballTask : TaskController
         {
             storedCustomer.SetAnimation("WallIdle", true);
         }
+        StartCoroutine(PlayTask());
+    }
+
+    IEnumerator PlayTask()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 5));
+        if (isBarrel)
+        {
+            storedCustomer.SetAnimation("BarrelShoot");
+        }
+        else
+        {
+            storedCustomer.SetAnimation("WallShoot");
+        }
+        yield return new WaitForSeconds(Random.Range(5, 7));
+        if (isBarrel)
+        {
+            storedCustomer.SetAnimation("BarrelShoot");
+        }
+        else
+        {
+            storedCustomer.SetAnimation("WallShoot");
+        }
+        yield return new WaitForSeconds(Random.Range(5, 7));
+        if (isBarrel)
+        {
+            storedCustomer.SetAnimation("BarrelShoot");
+        }
+        else
+        {
+            storedCustomer.SetAnimation("WallShoot");
+        }
+        yield return new WaitForSeconds(Random.Range(3, 5));
+        if (isBarrel)
+        {
+            storedCustomer.SetAnimation("BarrelIdle", false);
+        }
+        else
+        {
+            storedCustomer.SetAnimation("WallIdle", false);
+        }
+        storedCustomer.ExitCustomer();
+        storedCustomer = null;
     }
 }
